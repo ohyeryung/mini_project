10edafad52c48f33ff3324d7b5dc878b4f011760
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-06-08 23:45:39 +0900

    :tada: create project

---
+++ src/main/java/com/ohye/mini_pj/MiniPjApplication.java
@@ -0,0 +1,13 @@
+package com.ohye.mini_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniPjApplication.class, args);
+    }
+
+}
