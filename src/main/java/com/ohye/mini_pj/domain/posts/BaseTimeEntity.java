649e30aeb976a658cdc4e95ae23321e22e934b14
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-06-17 00:09:45 +0900

    :bug: chap 3-5 JPA Auditing으로 생성시간/수정시간 자동화_오류 수정

--- src/main/java/com/ohye/mini_pj/domain/posts/BaseTimeEntity.java
+++ src/main/java/com/ohye/mini_pj/domain/posts/BaseTimeEntity.java
@@ -3,6 +3,7 @@ package com.ohye.mini_pj.domain.posts;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AutoCloseable.class)
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
     @CreatedDate
