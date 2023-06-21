cc582605c4b2b7ad33003c101dea60115b64d499
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-06-21 17:22:36 +0900

    :sparkles: chap 5-3. 구글 소셜 로그인 기능 구현

--- src/main/java/com/ohye/mini_pj/web/IndexController.java
+++ src/main/java/com/ohye/mini_pj/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ohye.mini_pj.web;
 
+import com.ohye.mini_pj.config.auth.dto.SessionUser;
 import com.ohye.mini_pj.service.PostsService;
 import com.ohye.mini_pj.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
